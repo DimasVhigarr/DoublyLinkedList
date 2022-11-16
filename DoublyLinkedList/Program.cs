89bf2b1707caf51cc180d5fbe4aeba0f00ce6a20
DimasVhigarr <dimasvhigar2002@gmail.com>
2022-11-16 16:27:00 +0700

    membuat method class doublelinkedlist

--- DoublyLinkedList/Program.cs
+++ DoublyLinkedList/Program.cs
@@ -1,19 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DoublyLinkedList
 {
-    class node
+    class Node
     {
         public int rollNumber;
         public string name;
-        public node next;
-        public node prev;
+        public Node next;
+        public Node prev;
     }
+    class DoubleLinkedList
+    {
+        Node START;
+        public DoubleLinkedList()
+        {
+            START = null;
+        }
+        public void addNode()
+        {
+            int rollNo;
+            string nm;
+            Console.WriteLine("\nEnter the roll number of the student: ");
+            rollNo = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nEnter the name of the student: ");
+            nm = Console.ReadLine();
+            Node newnode = new Node();
+            newnode.rollNumber = rollNo;
+            newnode.name = nm;
+            /*check if the list is empty*/
+            if (START == null || rollNo <= START.rollNumber)
+            {
+                if ((START != null) && (rollNo == START.rollNumber))
+                {
+                    Console.WriteLine("\nDuplicate roll numbers not allowed");
+                }
+                newnode.next = START;
+                if (START != null)
+                    START.prev = newnode;
+                newnode.prev = null;
+                START = newnode;
+                return;
+            }
+            Node previous, current;
+            for (current = previous = START; current != null && rollNo >= current.rollNumber; previous = current, current = current.next)
+            {
+                if (rollNo == current.rollNumber)
+                {
+                    Console.WriteLine("\nDuplicate roll numbers not allowed");
+                    return;
+                }
+            }
+            newnode.next = current;
+            newnode.prev = previous;
 
+            if (current == null)
+            {
+                newnode.next = null;
+                previous.next = newnode;
+                return;
+            }
+            current.prev = newnode;
+            previous.next = newnode;
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
